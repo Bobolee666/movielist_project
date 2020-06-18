07e8f38c09eed1e3f2d996c7ad6d530b8b85958a
Bo Lee <31890461+bobolee666@users.noreply.github.com>
2020-06-18 12:35:13 -0500

    update App.js
    solve conflicts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-// import Home from "./components/HomePage";
-// import SideBar from "./components/SideBar";
 import MovieList from "./components/MovieList.jsx";
-=======
 import HomePage from "./components/HomePage";
-// import SideBar from "./components/SideBar";
->>>>>>> dca9ca6f5abea74608fe42319f7d4b67a12323be
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <HomePage /> */}
-      {/* <SideBar /> */}
       <MovieList />
-=======
       <HomePage />
->>>>>>> dca9ca6f5abea74608fe42319f7d4b67a12323be
     </div>
   );
 }
