ea4c9aabb010fdc97638b96426273fd340fd1b1c
洲帅陈 <zhoushuaichen@zhoushuaichen-MacBook-Pro.local>
2020-06-18 12:50:19 -0500

    I added my LikeList, please check it very carefully

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import MovieList from "./components/MovieList.jsx";
 import HomePage from "./components/HomePage";
+import LikeList from "./components/LikeList";
 
 function App() {
   return (
     <div className="App">
       <MovieList />
       <HomePage />
+      <LikeList />
     </div>
   );
 }
