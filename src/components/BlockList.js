f8360bf6d9a0232026a019bf560b115ace926902
HelenaXu <xuxue2291@itu.tsudents.edu>
2020-06-23 14:12:00 -0500

    Block and Like

--- src/components/BlockList.js
+++ src/components/BlockList.js
@@ -4,15 +4,15 @@ import "./Lists.css";
 
 export default class BlockList extends Component {
   render() {
-    const { blockedMovies, clickBlockBtn, clickLikeBtn } = this.props;
+    const { blockedMovies, clickBlockDelete, clickFav } = this.props;
     return (
       <div className="blockedMovie">
         {blockedMovies.map((movie) => (
           <BlockedItem
             key={movie.id}
             movie={movie}
-            clickBlockBtn={clickBlockBtn}
-            clickLikeBtn={clickLikeBtn}
+            clickBlockDelete={clickBlockDelete}
+            clickFav={clickFav}
           />
         ))}
       </div>
