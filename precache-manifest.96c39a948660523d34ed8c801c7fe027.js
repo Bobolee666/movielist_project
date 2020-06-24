297b69d32d4ab6ffef0a638016c92f70068d6d1a
Bohong Li <bohongbobo@gmail.com>
2020-06-24 13:41:43 -0500

    Updates

---
+++ precache-manifest.96c39a948660523d34ed8c801c7fe027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8441b4b5ec3baf21d7f46e212a5167",
+    "url": "/movielist_project/index.html"
+  },
+  {
+    "revision": "ae30a786c83b3a873f45",
+    "url": "/movielist_project/static/css/main.3a667b21.chunk.css"
+  },
+  {
+    "revision": "c4a546b5d70612f5effa",
+    "url": "/movielist_project/static/js/2.73e5d733.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movielist_project/static/js/2.73e5d733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae30a786c83b3a873f45",
+    "url": "/movielist_project/static/js/main.1c395688.chunk.js"
+  },
+  {
+    "revision": "fae8c7c5c473a1e6d247",
+    "url": "/movielist_project/static/js/runtime-main.30a58378.js"
+  }
+]);
